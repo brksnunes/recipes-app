4044f678ad083dd759885b929f125e2e27196ba4
DannOut <danielxdn@gmail.com>
2022-09-26 11:45:01 -0300

    mock repair

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -59,7 +59,7 @@ describe(' Testing Login Page with components', () => {
     expect(GetEmailFromLocalStorage.email).toBe(EMAIL_TEST);
 
     //! verificar o porque do erro ao renderizar /meals
-    // const { location: { pathname } } = history;
-    // expect(pathname).toBe('/meals');
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/');
   });
 });
