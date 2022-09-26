f89f1cb041f2d56fd7b9d80240bb5e41417fcfc1
DannOut <danielxdn@gmail.com>
2022-09-26 11:43:58 -0300

    questão 19 finalizada

--- src/tests/Footer.test.js
+++ src/tests/Footer.test.js
@@ -1,21 +1,30 @@
-// import React from 'react';
-// import { screen } from '@testing-library/react';
-// // import userEvent from '@testing-library/user-event';
-// // import App from '../App';
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import App from '../App';
 // import Meals from '../pages/Meals';
-// import renderWithRouter from '../helpers/renderWithRouter';
-// import { TEST_ID_FOOTER_MEALS, TEST_ID_FOOTER_DRINKS } from '../helpers/constants';
-// // import beefMeals from '../../cypress/mocks/beefMeals';
-// // import cocktailDrinks from '../../cypress/mocks/cocktailDrinks';
-// // import { MEALS_URL_BASE, DRINKS_URL_BASE } from '../helpers/constants';
+import renderWithRouter from '../helpers/renderWithRouter';
+import { TEST_ID_FOOTER_MEALS, TEST_ID_FOOTER_DRINKS } from '../helpers/constants';
+import beefMeals from '../../cypress/mocks/beefMeals';
+// import cocktailDrinks from '../../cypress/mocks/cocktailDrinks';
+// import { MEALS_URL_BASE, DRINKS_URL_BASE } from '../helpers/constants';
 
-// describe('Testing Footer Page with components', () => {
-//   test('Footer is rendered', async () => {
-//     renderWithRouter(<Meals />);
-//     const mealsFooter = await screen.getByTestId(TEST_ID_FOOTER_MEALS);
-//     const drinkFooter = await screen.getByTestId(TEST_ID_FOOTER_DRINKS);
+describe('Testing Footer Page with components', () => {
+  test('Footer is rendered', async () => {
+    const { history } = renderWithRouter(<App />, '/meals');
 
-//     expect(mealsFooter).toBeInTheDocument();
-//     expect(drinkFooter).toBeInTheDocument();
-//   });
-// });
+    jest.spyOn(global, 'fetch');
+    global.fetch.mockResolvedValue({
+      json: jest.fn().mockResolvedValue(beefMeals),
+    });
+
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/meals');
+
+    const mealsFooter = await screen.findByTestId(TEST_ID_FOOTER_MEALS);
+    const drinkFooter = await screen.getByTestId(TEST_ID_FOOTER_DRINKS);
+
+    expect(mealsFooter).toBeInTheDocument();
+    expect(drinkFooter).toBeInTheDocument();
+  });
+});
