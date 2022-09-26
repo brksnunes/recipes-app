7c4a8ce2634648e2bc7c334c533865b19cfaadff
DannOut <danielxdn@gmail.com>
2022-09-26 10:43:34 -0300

    questão 19 finalizada

--- src/tests/Footer.test.js
+++ src/tests/Footer.test.js
@@ -1,21 +1,19 @@
 // import React from 'react';
 // import { screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
-// import App from '../App';
+// // import userEvent from '@testing-library/user-event';
+// // import App from '../App';
+// import Meals from '../pages/Meals';
 // import renderWithRouter from '../helpers/renderWithRouter';
 // import { TEST_ID_FOOTER_MEALS, TEST_ID_FOOTER_DRINKS } from '../helpers/constants';
-// import beefMeals from '../../cypress/mocks/beefMeals';
-// import cocktailDrinks from '../../cypress/mocks/cocktailDrinks';
-// import { MEALS_URL_BASE, DRINKS_URL_BASE } from '../helpers/constants';
+// // import beefMeals from '../../cypress/mocks/beefMeals';
+// // import cocktailDrinks from '../../cypress/mocks/cocktailDrinks';
+// // import { MEALS_URL_BASE, DRINKS_URL_BASE } from '../helpers/constants';
 
 // describe('Testing Footer Page with components', () => {
-//   beforeEach(() => {
-
-//   });
-//   test('Footer is rendered', () => {
-//     renderWithRouter(<App />, '/meals');
-//     const mealsFooter = screen.getByTestId(TEST_ID_FOOTER_MEALS);
-//     const drinkFooter = screen.getByTestId(TEST_ID_FOOTER_DRINKS);
+//   test('Footer is rendered', async () => {
+//     renderWithRouter(<Meals />);
+//     const mealsFooter = await screen.getByTestId(TEST_ID_FOOTER_MEALS);
+//     const drinkFooter = await screen.getByTestId(TEST_ID_FOOTER_DRINKS);
 
 //     expect(mealsFooter).toBeInTheDocument();
 //     expect(drinkFooter).toBeInTheDocument();
