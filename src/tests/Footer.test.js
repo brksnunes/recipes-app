d5db498ce30a4732fc13d112c3714ffb64a9cab1
DannOut <danielxdn@gmail.com>
2022-09-26 10:16:56 -0300

    questão 19 finalizada

--- src/tests/Footer.test.js
+++ src/tests/Footer.test.js
@@ -4,8 +4,14 @@
 // import App from '../App';
 // import renderWithRouter from '../helpers/renderWithRouter';
 // import { TEST_ID_FOOTER_MEALS, TEST_ID_FOOTER_DRINKS } from '../helpers/constants';
+// import beefMeals from '../../cypress/mocks/beefMeals';
+// import cocktailDrinks from '../../cypress/mocks/cocktailDrinks';
+// import { MEALS_URL_BASE, DRINKS_URL_BASE } from '../helpers/constants';
 
 // describe('Testing Footer Page with components', () => {
+//   beforeEach(() => {
+
+//   });
 //   test('Footer is rendered', () => {
 //     renderWithRouter(<App />, '/meals');
 //     const mealsFooter = screen.getByTestId(TEST_ID_FOOTER_MEALS);
